378bebd25f95e0455e13e35cbbc5c922b5dc0bdc
Melissa Sweetwood <melissasweetwood@Melissas-MBP-2.nc.rr.com>
2018-12-07 18:40:11 -0500

    bye

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -13,25 +13,17 @@ public class PercolationDFSFast extends PercolationDFS {
 		}
 		//all of these else if's make sure current cell is in bounds
 		//if current cell is full, do dfs on the current cell
-		else if (col - 1 >= 0) {
-			if (isFull(row, col -1)) {
-				dfs(row, col);
-			}
+		else if (col - 1 >= 0 && isFull(row, col -1)){
+				dfs(row, col);	
 		}
-		else if (col + 1 < myGrid.length) {
-			if (isFull(row, col + 1)) {
+		else if (col + 1 < myGrid.length && isFull(row, col + 1)) {
 				dfs(row, col);
-			}
 		}
-		else if (row - 1 >= 0) {
-			if (isFull(row - 1, col)) {
+		else if (row - 1 >= 0 && isFull(row - 1, col)) {
 				dfs(row, col);
-			}
 		}
-		else if (row + 1 < myGrid.length) {
-			if (isFull(row + 1, col)) {
+		else if (row + 1 < myGrid.length && isFull(row + 1, col)) {
 				dfs(row, col);
-			}
 		}
 		
 	}
